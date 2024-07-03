232b2c2e2f4357ad4df1f71a5a862ab7deda3b8c
Mas'ud <68595786+dev-onimoe@users.noreply.github.com>
2024-07-03 10:40:59 +0100

    Initial Commit

---
+++ PaystackChallenge/PaystackChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaystackChallengeApp.swift
+//  PaystackChallenge
+//
+//  Created by Masud Onikeku on 03/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct PaystackChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
